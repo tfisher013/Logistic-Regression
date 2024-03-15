e30e1784b6b849a5f33e975cf2dabc9dbd41cd5b
Thomas Fisher <tfisher013@gmail.com>
2024-03-15 11:33:16 -0600

    Added consts file

---
+++ utils/consts.py
@@ -0,0 +1,15 @@
+# the percentage error to allow before terminating
+# gradient descent
+error_threshold = 0.01
+
+# step size used for gradient descent
+eta = 1.0
+
+# number of features extracted from audio files
+num_features = 20
+
+# the directory used to hold the feature CSV file
+feature_file_dir = 'feature_files'
+
+# the directory used to hold trained model files
+model_dir = 'models'
