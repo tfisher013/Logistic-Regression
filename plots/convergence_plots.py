e6e804ce350d75ce20eb60b9cba5faa41f7be566
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2024-04-10 19:03:28 -0600

    Code cleanup for plots/plot_convergence_rates

--- plots/convergence_plots.py
+++ plots/convergence_plots.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import csv
 
 def plot_convergence_by_step_size():
+    """ Plots 3 rates of convergence for different step sizes on the same graph
+    """
 
     # read in loss data
     loss_eta_point_one = []
@@ -22,20 +24,16 @@ def plot_convergence_by_step_size():
         for row in reader:
             loss_eta_two.append(float(row[0]))
 
-    # plot
+    # plot convergence rates
     plt.plot(range(len(loss_eta_point_one)), loss_eta_point_one, label='η = 0.1', color='blue')
     plt.plot(range(len(loss_eta_one)), loss_eta_one, label='η = 1.0', color='red')
     plt.plot(range(len(loss_eta_two)), loss_eta_two, label='η = 2.0', color='green')
 
-    # Set plot title and labels
+    # format and show plot
     plt.title('Change in Convergence Rate with Step Size')
     plt.xlabel('# Iterations')
     plt.ylabel('Loss')
-
-    # Display legend
     plt.legend()
-
-    # Display plot
     plt.grid(True)
     plt.tight_layout()
     plt.show()
