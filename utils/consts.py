56d00d8830902ad77e3dd33cc0e06b0a1ccf12d8
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2024-04-10 19:10:55 -0600

    Removed unused constants

--- utils/consts.py
+++ utils/consts.py
@@ -1,7 +1,4 @@
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-# the percentage error to allow before terminating
-# gradient descent
+# the amount of error to allow before terminating gradient descent
 epsilon = 0.01
 
 # step size used for gradient descent
@@ -10,39 +7,8 @@ eta = 1
 # regularization penalty
 lambda_hyperparameter = 0.00001
 
-# number of features extracted from audio files
-num_features = 40
-
-# the max number of iterations of gradient descent before
-# it is stopped
+# the max number of iterations of gradient descent before it is stopped
 max_iterations = 120000
-
-
-# the directory used to hold the feature CSV file
-feature_file_dir = 'feature_files'
 
 # the directory used to hold trained model files
 model_dir = 'models'
-
-training = 'train'
-
-testing = 'test'
-
-columns = list(range(0 , num_features+2))
-
-model = 'model.csv'
-
-others = 'others'
-
-true = 'true'
-
-feature_file_dir_test = 'feature_files_test'
-
-#making standardization global is good for transforming the real testing prediction data
-sc = StandardScaler()
-
-#making pca global is good for same reasons
-pca = PCA(n_components=0.7)
-
-# dictionary to map PCA objects to featuresets
-feature_extraction_method_dict = dict()
